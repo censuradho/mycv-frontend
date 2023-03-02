4217a0d48935a7c079663586aa9f441d5a180a0e
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-03-02 15:09:05 -0300

    chore(analytics)

--- src/config/appSettings.ts
+++ src/config/appSettings.ts
@@ -2,6 +2,7 @@ export const appSettings = {
   appName: process.env.NEXT_PUBLIC_APP_NAME || '@generic-name',
   siteUrl: process.env.SITE_URL || (process.env.VERCEL_URL && `https://${process.env.VERCEL_URL}`) || process.env.URL || 'http://localhost:3000',
   backend_url: process.env.NEXT_PUBLIC_BACKEND_URL,
-  ninjaApiKey: process.env.API_NINJAS_KEY
+  ninjaApiKey: process.env.API_NINJAS_KEY,
+  analytics: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS
 } 
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 
 
+import { appSettings } from '@/config/appSettings';
 import { AuthProvider } from '@/context/auth'
 import type { AppProps } from 'next/app'
 import dynamic from 'next/dynamic'
 import { useEffect } from 'react'
+
+import reactGA from "react-ga4";
 
 import { globalStyle } from 'stitches.config'
 
@@ -14,6 +17,10 @@ const ToastProvider = dynamic(() => import('@/context').then(c => c.ToastProvide
 export default function MyApp ({ Component, pageProps }: AppProps) {
   useEffect(() => {
     globalStyle()
+
+    if (process.env.NODE_ENV === 'production' && appSettings.analytics) {
+      reactGA.initialize(appSettings.analytics);
+    }
 
   }, [])
 
