d713b6fbc12ffc90af205005f4d4d3239ac9fd32
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-03-05 22:29:22 -0300

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
+import NextHead from 'next/head'
+
 import { Head } from "@/components/common";
 import { HomeLayout } from "@/layout/home";
 
 export default function HomePage () {
   return (
     <>
-      <Head 
-        title="MYCV | Seja encontrado, tenha um currículo online"
-        description="Encontre seu currículo ou daquele profissional que você está procurando" 
-      />
+      <NextHead>
+        <Head 
+          title="MYCV | Seja encontrado, tenha um currículo online"
+          description="Encontre seu currículo ou daquele profissional que você está procurando" 
+        />
+      </NextHead>
       <HomeLayout />
     </>
   )
