c866f4a532983a61911df651f3e2f534435a9ce5
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-03-16 08:35:46 -0300

    fix(deploy build)

---
+++ src/components/common/icon/icons.ts
@@ -0,0 +1,126 @@
+import {
+  AiFillLinkedin,
+  AiFillLock,
+  AiFillPhone,
+  AiFillTag,
+  AiOutlineArrowLeft,
+  AiOutlineClose, AiOutlineDelete, AiOutlineHome, AiOutlineMail, AiOutlinePlus, AiOutlineUser
+} from '@meronex/icons/ai';
+import {
+  BiCodeAlt, BiCommand, BiDotsVerticalRounded, BilFacebookSquare, BilGithub,
+  BilInstagramAlt, BiListOl, BilSpotify, BilTwitter, BisDashboard,
+  BisDownArrow, BisFilePdf, BisReport, BisShareAlt, BisUpArrow, BisUserCircle
+} from '@meronex/icons/bi';
+import {
+  EnLocationPin,
+  EnPinterest
+} from '@meronex/icons/en';
+import {
+  FaBehanceSquare,
+  FaBox,
+  FaDatabase, FaOpencart, FaStore, FaSuitcase, FaTelegramPlane
+} from '@meronex/icons/fa';
+import {
+  IosArrowDown,
+  IosArrowUp,
+  IosTrendingUp,
+  LogoWhatsapp
+} from '@meronex/icons/ios';
+import {
+  RiFileCopyLine, RiNavigationLine, RiNewspaperLine, RiShareBoxLine
+} from '@meronex/icons/ri';
+import {
+  VscMenu
+} from '@meronex/icons/vsc';
+
+import {
+  BsHeartFill,
+  BsListUl,
+  BsQuestion, BsSearch, BsTypeBold, BsTypeItalic, BsTypeStrikethrough, BsTypeUnderline
+} from '@meronex/icons/bs';
+import {
+  EnRss
+} from '@meronex/icons/en';
+
+import {
+  BsCodeSlash, BsFillEyeFill, BsFillEyeSlashFill
+} from '@meronex/icons/bs';
+import {
+  CgGym, CgMenuLeft
+} from '@meronex/icons/cg/';
+import { GrDocumentDownload, GrWheelchair } from '@meronex/icons/gr';
+import { MdAttachMoney, MdEdit } from '@meronex/icons/md';
+import {
+  MdcClockFast, MdcPokerChip, MdcWeb
+} from '@meronex/icons/mdc';
+
+
+export const icons = {
+  edit: MdEdit,
+  documentDownload: GrDocumentDownload,
+  headert: BsHeartFill,
+  userCircle: BisUserCircle,
+  search: BsSearch,
+  add: AiOutlinePlus,
+  suiteCase: FaSuitcase,
+  github: BilGithub,
+  instagram: BilInstagramAlt,
+  mail: AiOutlineMail,
+  linkedin: AiFillLinkedin,
+  shareAlt: BisShareAlt,
+  facebook: BilFacebookSquare,
+  twitter: BilTwitter,
+  shareBoxLine: RiShareBoxLine,
+  whatsapp: LogoWhatsapp,
+  outlineClose: AiOutlineClose,
+  location: EnLocationPin,
+  codeAlt: BiCodeAlt,
+  rss: EnRss,
+  spotify: BilSpotify,
+  menu: VscMenu,
+  clock: MdcClockFast,
+  code: BsCodeSlash,
+  database: FaDatabase,
+  user: AiOutlineUser, 
+  web: MdcWeb,
+  command: BiCommand,
+  copy: RiFileCopyLine,
+  home: AiOutlineHome,
+  poker: MdcPokerChip,
+  gym: CgGym,
+  eyeClose: BsFillEyeSlashFill,
+  eye: BsFillEyeFill,
+  product: FaOpencart,
+  dashboard: BisDashboard,
+  menuLeft: CgMenuLeft ,
+  arrowDropDown: BisDownArrow,
+  arrowDropUp: BisUpArrow,
+  pdf: BisFilePdf ,
+  report: BisReport,
+  lock: AiFillLock,
+  trendingUp: IosTrendingUp,
+  money: MdAttachMoney,
+  close: AiOutlineClose,
+  store: FaStore,
+  tag: AiFillTag,
+  digital: RiNavigationLine,
+  physical: FaBox,
+  arrowLeft: AiOutlineArrowLeft,
+  question: BsQuestion,
+  arrowDown: IosArrowDown,
+  arrowUp: IosArrowUp,
+  verticalDot: BiDotsVerticalRounded,
+  delete: AiOutlineDelete,
+  phone: AiFillPhone,
+  telegram: FaTelegramPlane,
+  bold: BsTypeBold,
+  italic: BsTypeItalic,
+  underline: BsTypeUnderline,
+  lineThrough: BsTypeStrikethrough,
+  listOl: BiListOl,
+  listUl: BsListUl,
+  behance: FaBehanceSquare,
+  pinterest: EnPinterest,
+  paperLine: RiNewspaperLine,
+  wheelChair: GrWheelchair
+}
