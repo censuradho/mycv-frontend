b8b761b70532cf659e03635a8b9cebd92a38d190
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-03-21 13:06:50 -0300

    refactor(dependencies)

--- src/components/common/markdown-editor/types.ts
+++
@@ -1,7 +0,0 @@
-import { ChangeEvent } from "react";
-
-export interface MarkDownEditorProps {
-  onChange?: (data: { text: string; html: string }, event?: ChangeEvent<HTMLTextAreaElement> | undefined) => void | undefined
-  label?: string
-  errorMessage?: string
-}
